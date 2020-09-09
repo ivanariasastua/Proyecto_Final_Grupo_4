cc7b0975f6977e391276e87aca399f6752430acb
YohellBuzo <54456019+YohellBuzo@users.noreply.github.com>
2020-09-09 16:32:48 -0600

    Configuración API
    Dependencia modelmapper
    Configuración del Swagger

---
+++ api/aeropuerto/src/main/java/org/una/aeropuerto/SwaggerConfiguration.java
@@ -0,0 +1,70 @@
+package org.una.aeropuerto;
+
+import static io.swagger.annotations.ApiKeyAuthDefinition.ApiKeyLocation.HEADER;
+import java.util.Collections;
+import static java.util.Collections.singletonList;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author Dios
+ */
+
+@Configuration
+@EnableSwagger2
+
+public class SwaggerConfiguration {
+
+@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(singletonList(new ApiKey("JWT", AUTHORIZATION, HEADER.name())))
+                .securityContexts(singletonList(
+                        SecurityContext.builder()
+                                .securityReferences(
+                                        singletonList(SecurityReference.builder()
+                                                .reference("JWT")
+                                                .scopes(new AuthorizationScope[0])
+                                                .build()
+                                        )
+                                )
+                                .build())
+                )
+                .select()
+                .apis(
+                        RequestHandlerSelectors
+                                .basePackage("org.una.aeropuerto.controllers"))
+                .paths(PathSelectors.regex("/.*"))
+                .build()
+                .apiInfo(apiInfo())
+                .tags(new Tag("Seguridad", "Metodos de Seguridad"),
+                        new Tag("Usuarios", "Entidad de Usuarios")
+                );
+
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Aeropuerto",
+                "Rest API sobre el Aeropuerto.",
+                "Versión:2.1.0",
+                "https://google.com",
+                new Contact("UNA Sede Región Brunca", "https://srb.una.ac.cr/index.php/es/", "decanatosrb@una.cr") {},
+                "Apache-2.0", "http://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
+    }
+
+}
