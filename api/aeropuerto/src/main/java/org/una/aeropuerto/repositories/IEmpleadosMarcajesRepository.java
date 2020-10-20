4061a13c8a462e8009885a88e3c747b3ff68b029
ivanariasastua <69368731+ivanariasastua@users.noreply.github.com>
2020-10-19 23:53:49 -0600

    Update IEmpleadosMarcajesRepository.java

--- api/aeropuerto/src/main/java/org/una/aeropuerto/repositories/IEmpleadosMarcajesRepository.java
+++ api/aeropuerto/src/main/java/org/una/aeropuerto/repositories/IEmpleadosMarcajesRepository.java
@@ -5,7 +5,6 @@
  */
 package org.una.aeropuerto.repositories;
 
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
